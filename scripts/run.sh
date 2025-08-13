53a4cd3072ae27195f393ef35c760f23e0d55e15
Pavel Sergeev <pasergeev@luxoft.com>
2025-08-13 16:39:46 +0200

    fix passing parameters to executeTests.py

--- scripts/run.sh
+++ scripts/run.sh
@@ -5,8 +5,8 @@ TESTS_FILTER="$2"
 python3.9 -m pip install -r ../jobs_launcher/install/requirements.txt
 
 python3.9 ../jobs_launcher/executeTests.py \
-    --test_filter "$TESTS_FILTER" \
-    --file_filter "$FILE_FILTER" \
+    --test_filter $TESTS_FILTER \
+    --file_filter $FILE_FILTER \
     --tests_root ../jobs \
     --work_root ../Work/Results \
     --work_dir Xilinx \
